fb310b01245eab3a4db1dacf7c2a419d9cc94630
antocecere <antocecere77@gmail.com>
2020-12-07 15:28:03 +0100

    Changed credentials id

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -16,7 +16,7 @@ job('NodeJS Docker example') {
         dockerBuildAndPublish {
             repositoryName('antocecere77/docker-nodejs-demo')
             tag('${GIT_REVISION,length=9}')
-            registryCredentials('DockerHub')
+            registryCredentials('dockerhub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
